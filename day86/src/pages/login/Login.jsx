5b2180ac8f3c7c48ea075ac75f47e7d2e91ca48e
Suyan Shrestha <99606480+SuyanShrestha@users.noreply.github.com>
2024-03-18 20:36:38 +0545

    Add files via upload

---
+++ day86/src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./login.scss";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>मेरो Nexus.</h1>
+          <p>
+            Ready to jump back into the action? Log in to reconnect with
+            friends, explore the latest trends, and share your stories!
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
